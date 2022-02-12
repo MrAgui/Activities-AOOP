d48a5f8f6e69360ed0562a75cf996558a9e39658
MrAgui <karlaguilar3800@gmail.com>
2022-02-12 11:26:19 +0800

    Activity Palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args){
+        String input, output;
+
+        Scanner scanner = new Scanner(System.in);
+/*Get input from user*/
+        System.out.println("Enter word or number for checking a palindrome: ");
+        input = scanner.nextLine();
+
+
+
+
+    }
+}
